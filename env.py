a2b22cf2d4cc9df70f3e4b228d229752053efb91
Delviet <59310708+Delviet@users.noreply.github.com>
2020-01-02 03:01:56 +0300

    GPU for playground

--- env.py
+++ env.py
@@ -1,7 +1,8 @@
 import numpy as np
-
+from numba import jit
 
 class Playground:
+    
     
     def __init__(self, height, width, alpha = 0.2):
         
@@ -16,6 +17,7 @@ class Playground:
         
         typical_shape = (self.height, self.width)
         
+        
         self.mines = np.zeros(typical_shape, dtype = 'bool')
         self.place_mines()
         
@@ -24,26 +26,10 @@ class Playground:
         self.player_field = np.full(typical_shape, '*')
         
     def place_mines(self):
-        self.amount_of_mines = int(np.round(self.square * self.alpha))
-        positions = np.arange(self.square)
-        np.random.shuffle(positions)
-        for pos in positions[:self.amount_of_mines]:
-            self.mines[pos // self.width, pos % self.width] = True
+        self.amount_of_mines = place_mines(self.height, self.width, self.alpha, self.mines)
     
     def find_values(self):
-        temp_vals = np.zeros((self.height + 2, self.width + 2))
-        mask = np.ones((3,3))
-        mask[1][1] = 0
-        for y in range(self.height):
-            for x in range(self.width):
-                if self.mines[y, x]:
-                    temp_vals[y:y+3, x:x+3] += mask
-                    
-        for y in range(self.height):
-            for x in range(self.width):
-                if self.mines[y, x]:
-                    temp_vals[y+1, x+1] = -1
-        self.values = temp_vals[1:-1, 1:-1]
+        self.values = find_values(self.height, self.width, self.mines)
     
     def show(self):
         print()
@@ -73,6 +59,33 @@ class Playground:
             
         return result
 
+    
+@jit(nopython=True)
+def place_mines(height, width, alpha, mines):
+    amount_of_mines = int(np.round(height * width * alpha))
+    positions = np.arange(height * width)
+    np.random.shuffle(positions)
+    for pos in positions[:amount_of_mines]:
+        mines[pos // width, pos % width] = True
+        
+    return amount_of_mines
+
+@jit(nopython=True)
+def find_values(height, width, mines):
+    temp_vals = np.zeros((height + 2, width + 2))
+    mask = np.ones((3,3))
+    mask[1][1] = 0
+    for y in range(height):
+        for x in range(width):
+            if mines[y, x]:
+                temp_vals[y:y+3, x:x+3] += mask
+                    
+    for y in range(height):
+        for x in range(width):
+            if mines[y, x]:
+                temp_vals[y+1, x+1] = -1
+                
+    return temp_vals[1:-1, 1:-1]
 
 class Game:
     
